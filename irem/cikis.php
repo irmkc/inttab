677d3ea379a60d4940d3481560d635c271cc824a
irmkc <149599205+irmkc@users.noreply.github.com>
2023-12-28 18:31:28 +0300

    i
    i

--- irem/cikis.php
+++
@@ -1,8 +0,0 @@
-<?php
-setcookie("yetki", false, time()-1);
-setcookie("ad","", time()-1);
-setcookie("soyad", "", time()-1);
-setcookie("user_id", "", time()-1);
-echo "Çıkış başarılı!";
-header("Refresh:1; url=anasayfa.php");
-?>
